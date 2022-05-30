4199968a873ea35b751ff1b53a71b5a8e76a9aa3
Devhazemmahmoud1 <dev.hazemmahmoud@gmail.com>
2022-05-31 00:07:33 +0200

    hello world

--- controllers/socialMediaControllers/myProfileController.js
+++ controllers/socialMediaControllers/myProfileController.js
@@ -616,7 +616,7 @@ let getMainPage = async (req, res) => {
     })
 
     for (item of getMyFriends) {
-        console.log(item)
+        console.log(item.user.posts)
         item.user.posts.userInfo = await db.users.findFirst({
             where: {
                 id: item.user.posts.user_id
@@ -626,7 +626,7 @@ let getMainPage = async (req, res) => {
     }
 
     for (item of getMyFollowing) {
-        console.log(item)
+        console.log(item.user.posts)
         item.user.posts.userInfo = await db.users.findFirst({
             where: {
                 id: item.user.posts.user_id
