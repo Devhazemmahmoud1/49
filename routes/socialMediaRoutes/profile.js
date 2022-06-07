27363385babb3570653d481e5905263d3435d8d1
Devhazemmahmoud1 <dev.hazemmahmoud@gmail.com>
2022-06-07 09:43:06 +0200

    hello world

--- routes/socialMediaRoutes/profile.js
+++ routes/socialMediaRoutes/profile.js
@@ -52,6 +52,14 @@ router.post('/upload-profile-picture', upload.array('attachments', 12), async (r
 
         let createNewAd = await db.posts.create({
             data: {
+                post_content: title ?? '',
+                total_comments: 0,
+                total_reactions: 0,
+                feeling_id: feeling != null ? parseInt(feeling) : 0,
+                activity_id: activity != null ? parseInt(activity) : 0,
+                location: location ?? '',
+                lat: lat ?? '',
+                lng: lng ?? '',
                 type: 1,
                 user_id: parseInt(req.body.id)
             }
