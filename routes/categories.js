cf42d6582aaef8737456c28d0dab26a41a22c5d2
Devhazemmahmoud1 <dev.hazemmahmoud@gmail.com>
2022-06-17 01:39:58 +0200

    hello world

--- routes/categories.js
+++ routes/categories.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var categories = require('../controllers/CategoriesController')
 const {autoCatch} = require("../utils/auto_catch");
 
-router.get('/all',autoCatch( categories.getAllCategories))
+router.get('/all', guard, autoCatch( categories.getAllCategories))
 
-router.get('/sub/:id',autoCatch( categories.getSubCats))
+router.get('/sub/:id', guard, autoCatch( categories.getSubCats))
 
 module.exports = router
