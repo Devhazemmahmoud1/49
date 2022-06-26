2e1be3e967ca8225b0bd1ea352eefdd7f287885e
Devhazemmahmoud1 <dev.hazemmahmoud@gmail.com>
2022-06-27 01:31:30 +0200

    hello world

--- controllers/walletController.js
+++ controllers/walletController.js
@@ -3,7 +3,11 @@ const { PrismaClient } = require('@prisma/client')
 const db = new PrismaClient();
 
 let getWallet = async (req, res) => {
-    
+    return res.json(await db.wallet.findFirst({
+        where: {
+            user_id: req.user.id
+        }
+    }))
 }
 
 module.exports = { getWallet }
