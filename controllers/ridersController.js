b33f1cc5b985a5407dd2958cd00eded01c229ff8
Devhazemmahmoud1 <dev.hazemmahmoud@gmail.com>
2022-06-28 03:50:06 +0200

    hello world

--- controllers/ridersController.js
+++ controllers/ridersController.js
@@ -148,7 +148,7 @@ let findRiders = async (req, res) => {
                                 continue;
                             }
                         }
-                        
+
                         global.io.to(sockets[rider].socket_id).emit('request', JSON.stringify({
                             rider_id: sockets[rider].user_id,
                             user_id: req.user.id,
@@ -632,7 +632,7 @@ let acceptRide = async (req, res) => {
         distance,
         tripTime,
         customerLng,
-        customerlat,
+        customerLat,
         destinationLat,
         destinationLng,
         streetFrom,
