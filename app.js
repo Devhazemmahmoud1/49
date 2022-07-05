53860bcfb0751ea597cbb835ad04047e3265e92e
Hazem mahmoud <63607528+Devhazemmahmoud1@users.noreply.github.com>
2022-07-05 23:34:40 +0200

    hello world

--- app.js
+++ app.js
@@ -101,8 +101,8 @@ app.use((req, res, next) => {
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'jade');
 
 //app.use(logger('dev'));
 app.use(express.json());
